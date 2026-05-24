13a8c7b299dc66f067c549f4bae6d731a44cac3d
wiushonka <bagrato2004@gmail.com>
2026-05-24 21:55:16 +0200

    smolsmolimprovement

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/user-/OneDrive/Desktop/bimbim/2D_fast_with_cols.c" "CMakeFiles/main.dir/2D_fast_with_cols.c.obj" "gcc" "CMakeFiles/main.dir/2D_fast_with_cols.c.obj.d"
+  "C:/Users/user-/OneDrive/Desktop/bimbim/2D_fast.c" "CMakeFiles/main.dir/2D_fast.c.obj" "gcc" "CMakeFiles/main.dir/2D_fast.c.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
